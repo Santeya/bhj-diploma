79b6cbec782a467cd0317224632e61bb936afe45
Santeya <santeya@gmx.net>
2022-10-14 16:24:13 +0100

    Diplom part1

--- public/js/api/User.js
+++ public/js/api/User.js
@@ -1,47 +1,41 @@
-/**
- * Класс User управляет авторизацией, выходом и
- * регистрацией пользователя из приложения
- * Имеет свойство URL, равное '/user'.
- * */
+// Класс User управляет авторизацией, выходом и регистрацией пользователя из приложения
+// Имеет свойство URL, равное '/user'.
 class User {
-  /**
-   * Устанавливает текущего пользователя в
-   * локальном хранилище.
-   * */
+  static URL = '/user';
+ 
+  // Устанавливает текущего пользователя в локальном хранилище.
   static setCurrent(user) {
-
+    localStorage.user = JSON.stringify(user);
   }
 
-  /**
-   * Удаляет информацию об авторизованном
-   * пользователе из локального хранилища.
-   * */
+  // Удаляет информацию об авторизованном пользователе из локального хранилища.
   static unsetCurrent() {
-
+    localStorage.removeItem('user');
   }
 
-  /**
-   * Возвращает текущего авторизованного пользователя
-   * из локального хранилища
-   * */
+ // Возвращает текущего авторизованного пользователя из локального хранилища
   static current() {
-
+    return JSON.parse(localStorage.getItem('user'));
   }
 
-  /**
-   * Получает информацию о текущем
-   * авторизованном пользователе.
-   * */
+  // Получает информацию о текущем авторизованном пользователе.
   static fetch(callback) {
-
+    createRequest({
+      url: this.URL + '/current',
+      method: 'GET',
+      callback(err, response) {
+        if (response && response.user) {
+          this.setCurrent(response.user);
+        } else {
+          this.unsetCurrent();
+        }
+        callback(err, response);
+      }     
+    })
   }
 
-  /**
-   * Производит попытку авторизации.
-   * После успешной авторизации необходимо
-   * сохранить пользователя через метод
-   * User.setCurrent.
-   * */
+  // Производит попытку авторизации. После успешной авторизации необходимо
+  // сохранить пользователя через метод User.setCurrent.
   static login(data, callback) {
     createRequest({
       url: this.URL + '/login',
@@ -57,14 +51,20 @@ class User {
     });
   }
 
-  /**
-   * Производит попытку регистрации пользователя.
-   * После успешной авторизации необходимо
-   * сохранить пользователя через метод
-   * User.setCurrent.
-   * */
+  // Производит попытку регистрации пользователя. После успешной авторизации 
+  // необходимо сохранить пользователя через метод User.setCurrent.  
   static register(data, callback) {
-
+    createRequest({
+      url: this.URL + '/register',
+      method: 'POST',
+      data,
+      callback(err, response) {
+        if (response && response.user) {
+          this.setCurrent(response.user);
+        } 
+        callback(err, response);
+        }
+    })
   }
 
   /**
@@ -72,6 +72,14 @@ class User {
    * выхода необходимо вызвать метод User.unsetCurrent
    * */
   static logout(callback) {
-
+    createRequest({
+      url: this.URL + '/logout',
+      method: 'POST',
+      callback(err, response) {
+        if (response.success) {  //?
+          this.unsetCurrent();
+        }
+      }
+    })
   }
 }
--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,54 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    const { url, data, method, callback } = options;
+    const xhr = new XMLHttpRequest;
+    xhr.responseType = 'json';
 
-};
+    const formData = new FormData();
+   
+    if (method === 'GET') {
+        url += '?' + Object.entries(data).map(([key, value]) => `${key}=${value}`.join('&'));
+        formData = '';
+    } else {
+        Object.entries(data).forEach((key, value) => formData.append(key, value));
+    }
+
+    try {
+        xhr.open(method, url);
+        xhr.send(formData);
+    }
+    catch (err) {
+        callback(err, response);
+    }
+
+    xhr.addEventListener('readystatechange', function () {
+        if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(xhr.status, xhr.statusText);
+        }
+    })
+}
+
+/*
+// здесь перечислены все возможные параметры для функции
+createRequest({
+    url: 'https://example.com', // адрес
+    data: { // произвольные данные, могут отсутствовать
+        email: 'ivan@poselok.ru',
+        password: 'odinodin'
+    },
+    method: 'GET', // метод запроса
+    /*
+      Функция, которая сработает после запроса.
+      Если в процессе запроса произойдёт ошибка, её объект
+      должен быть в параметре err.
+      Если в запросе есть данные, они должны быть переданы в response.
+    
+    callback: (err, response) => {
+        console.log('Ошибка, если есть', err);
+        console.log('Данные, если нет ошибки', response);
+    }
+});
+*/
